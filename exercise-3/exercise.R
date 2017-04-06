d7fbbdbb2ce2877dedb6a6cb81f2ea27da37b092
Connie He <conniehe@D-108-179-166-169.dhcp4.washington.edu>
2017-04-06 13:52:10 -0700

    New

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,15 +1,24 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-
+marbles<-c("blue","red","green","yellow","blue","black")
 
 # Use the `sample` function to select a single marble
-
+selection<-sample(marbles,1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
+MarbleGame<-function(guess){
+  marbles<-c("blue","red","green","yellow","blue","black")
+  selection<-sample(marbles,1)
+  if(guess==selection){
+    return("You Got It!")
+  }else{
+    return("Sorry, it's wrong.")
+  }
+}
 
 
 # Play the marble game!
@@ -20,3 +29,17 @@
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
+guess<-"yellow"
+MarbleGame<-function(guess){
+  marbles<-c("blue","red","green","yellow","blue","black")
+  selection<-sample(marbles,1)
+  if(guess==selection){
+    return(1)
+    print("You Got It!")
+  }else{
+    return(0)
+    print("Sorry, it's wrong.")
+  }
+}
+bonus= replicate(10000,MarbleGame("yellow"),simplify="array")
+mean(bonus)
